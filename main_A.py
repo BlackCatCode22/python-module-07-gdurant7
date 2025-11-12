2361c67deea46e57f9c5e0216cf7a34be8f38481
Durant <gunnar.durant@protonmail.com>
2025-11-12 09:40:54 -0800

    Update main_A.py
    now works, and writes to txt file.  
    
    needs and eddited csv file for arriving animals, and names.  
    
    will work on one that can use original txt file given for assignment.
    
    output formating can use some work,

--- main_A.py
+++ main_A.py
@@ -1,17 +1,8 @@
-import ZooSubSounds
-from ZooA import ZooA
-from ZooSubSounds import hyena, lion, bear, tiger
+rom ZooA import ZooA
 
 
 animal_file = input('Enter File Name For Animals: ')
 name_file = input('Enter File Name For Names: ')
-ZooA_instance = ZooA(0, 'a', 'b', 0, 'c', 'd', 'e', 'f', 'g'  )
-ZooA_instance.files(animal_file)
-ZooA_instance.name_generator(name_file)
+ZooA_instance = ZooA()
+ZooA_instance.files(animal_file, name_file)
 ZooA.write_report()
-
-print("\n-----Example of Zoo Sounds-----\n")
-print(ZooSubSounds.hyena(0, 'a', 'b', 0, 'c', 'd', 'e', 'f', 'g' ))
-print(ZooSubSounds.lion(0, 'a', 'b', 0, 'c', 'd', 'e', 'f', 'g' ))
-print(ZooSubSounds.bear(0, 'a', 'b', 0, 'c', 'd', 'e', 'f', 'g' ))
-print(ZooSubSounds.tiger(0, 'a', 'b', 0, 'c', 'd', 'e', 'f', 'g' ))
