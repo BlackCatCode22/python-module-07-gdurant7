94adc4730e3d3707a0a95634b91c9cb777b94170
Durant <gunnar.durant@protonmail.com>
2025-11-12 10:30:44 -0800

    Update main_A.py
    added hint for file names

--- main_A.py
+++ main_A.py
@@ -1,8 +1,9 @@
 rom ZooA import ZooA
 
 
-animal_file = input('Enter File Name For Animals: ')
-name_file = input('Enter File Name For Names: ')
+animal_file = input('Enter File Name For Animals: ')  # animalsEdited.txt
+name_file = input('Enter File Name For Names: ')      #namesEdited.txt
 ZooA_instance = ZooA()
 ZooA_instance.files(animal_file, name_file)
 ZooA.write_report()
+
