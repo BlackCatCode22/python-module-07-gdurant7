23502b359ebb5636cffc73a1a9c43a021d0a0534
Durant <gunnar.durant@protonmail.com>
2025-11-12 16:01:44 -0800

    Update main_A.py

--- main_A.py
+++ main_A.py
@@ -1,4 +1,4 @@
-rom ZooA import ZooA
+from ZooA import ZooA
 
 
 animal_file = input('Enter File Name For Animals: ')  # animalsEdited.txt
@@ -6,4 +6,5 @@ name_file = input('Enter File Name For Names: ')      #namesEdited.txt
 ZooA_instance = ZooA()
 ZooA_instance.files(animal_file, name_file)
 ZooA.write_report()
+
 
